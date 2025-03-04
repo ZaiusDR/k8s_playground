b1a1d41b9d2e0163fba44c87451a13db2a57cd24
Eduardo Suarez <eduardosuarez@gmail.com>
2025-03-04 12:32:04 +0100

    Fix SG

--- infrastructure/pulumi/common_infra.py
+++ infrastructure/pulumi/common_infra.py
@@ -1,16 +1,17 @@
 import pulumi
 
-from pulumi_aws import ec2
+import pulumi_aws
 
-ec2.SecurityGroup('ssh-access-sg',
+ssh_sg = pulumi_aws.ec2.SecurityGroup('ssh-access-sg',
     name='ssh-access-sg',
     description='Security group for accessing by SSH',
     ingress=['allow-ssh-access']
 )
 
-ec2.SecurityGroupIngressRule('allow-ssh-access',
-    protocol='tcp',
+pulumi_aws.vpc.SecurityGroupIngressRule('allow-ssh-access',
+    security_group_id=ssh_sg.id,
+    cidr_ipv4='0.0.0.0/0',
     from_port=22,
-    to_port=22,
-    cidr_blocks=['0.0.0.0/0']
+    ip_protocol='tcp',
+    to_port=22
 )
